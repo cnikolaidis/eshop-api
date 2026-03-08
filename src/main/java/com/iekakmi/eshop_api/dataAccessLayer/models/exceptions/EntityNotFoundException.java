35e5b615e6c50e841f751ab733ae3d0b72ed455c
Christos Nikolaidis <c.nikolaidis.90@gmail.com>
2026-03-08 18:50:52 +0200

    Domain

--- src/main/java/com/iekakmi/eshop_api/dataAccessLayer/models/exceptions/EntityNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.iekakmi.eshop_api.dataAccessLayer.models.exceptions;
-
-public class EntityNotFoundException extends RuntimeException
-{
-	public EntityNotFoundException(String entityName, int id)
-	{
-		super(entityName + " not found with id: " + id);
-	}
-}
